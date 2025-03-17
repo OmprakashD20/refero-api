58f7e1d7e6aaf99a194d05265c0862ceeecf5570
OmprakashD20 <omugan07@gmail.com>
2025-03-17 20:08:29 +0530

    wip: remove excess comments

--- errors/db_errors.go
+++ errors/db_errors.go
@@ -17,18 +17,6 @@ var (
 )
 
 // IsErrNoRows checks if the provided error is a pgx.ErrNoRows error.
-// If so, it returns the provided default value and a nil error.
-//
-// This function is useful for handling database queries where an absence
-// of a row should not be treated as an error but rather as a valid case.
-//
-// Parameters:
-//   - err (error): The error to check.
-//   - value (T): The default value to return if the error is pgx.ErrNoRows.
-//
-// Returns:
-//   - T: The default value if pgx.ErrNoRows, otherwise the original value.
-//   - error: nil if pgx.ErrNoRows, otherwise the original error.
 func IsErrNoRows[T any](err error, value T) (T, error) {
 	if errors.Is(err, pgx.ErrNoRows) {
 		return value, nil
