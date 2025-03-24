2da106e6c00b889a41cff0e1a6d45f48b826e844
OmprakashD20 <omugan07@gmail.com>
2025-03-25 00:01:01 +0530

    fix: return 500 server error on encountering panics

--- middlewares/recovery.go
+++ middlewares/recovery.go
@@ -1,26 +1,26 @@
 package middlewares
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 
-	"github.com/OmprakashD20/refero-api/errors"
+	// "github.com/OmprakashD20/refero-api/errors"
 
 	"github.com/gin-gonic/gin"
 )
 
-
-// RecoveryMiddleware recovers from panics that occur during request processing. 
-// If a panic occurs, it sets an internal server error in the context. 
+// RecoveryMiddleware recovers from panics that occur during request processing.
+// If a panic occurs, it sets an internal server error in the context.
 // This prevents the server from getting crashed due to a panic.
 func RecoveryMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if rec := recover(); rec != nil {
 				log.Printf("PANIC RECOVERED: %v\n", rec)
-				err := errors.InternalServerError(errors.WithCause(fmt.Errorf("%v", rec)))
 
-				c.Error(err)
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"error": "Internal Server Error",
+				})
 			}
 		}()
 
